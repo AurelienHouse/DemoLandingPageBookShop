ae5e95704971b429a061217e8e963e2e3889ab06
Seth Gaara <83479322+Seth94800@users.noreply.github.com>
2024-09-23 01:54:13 +0200

    🚧 setup links to epagine.fr

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -18,15 +18,17 @@ export default function NavBar() {
           </button>
 
           <div className={`${isMenuOpen ? 'block' : 'hidden'} md:flex w-full md:w-auto md:space-x-6 mt-4 md:mt-0`}>
-            <Link href="/livres" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">Livres</Link>
-            <Link href="/auteurs" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">Auteurs</Link>
-            <Link href="/articles" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">Articles</Link>
-            <Link href="/a-propos" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">À propos</Link>
+            <Link href="https://www.epagine.fr/livre-audio-mp3/ssh-1387" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">Livres audio</Link>
+            <Link href="https://www.epagine.fr/listeliv.php?refgtl=home&base=ebook&select_tri_recherche=&codegtl1=90000000&codegtl2=90010000&rayon=Policier+%26amp%3B+Thriller%7CPolicier" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">Polars</Link>
+            <Link href="https://www.epagine.fr/listeliv.php?form_recherche_avancee=ok&getbase=ebook&is_promotion=1&base=ebook&dispo=1%2C2&select_tri_recherche=pertinence" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">Promos</Link>
+            <Link href="https://www.epagine.fr/infosprat.php" className="block md:inline-block hover:underline hover:text-blue-600 hover:underline-offset-4 transition duration-300 mb-2 md:mb-0">À propos</Link>
           </div>
 
           <div className={`${isMenuOpen ? 'block' : 'hidden'} md:flex items-center w-full md:w-auto mt-4 md:mt-0`}>
             <input type="text" placeholder="Je cherche un livre..." className="w-full md:w-auto mb-2 md:mb-0 md:mr-4 p-2 border rounded" />
-            <button className="w-full md:w-auto bg-gray-800 hover:bg-gray-600 text-white px-4 py-2 rounded">Contact</button>
+            <a href="https://www.epagine.fr/infosprat.php" aria-label="Contact">
+              <button className="w-full md:w-auto bg-gray-800 hover:bg-gray-600 text-white px-4 py-2 rounded">Contact</button>
+            </a>
           </div>
         </div>
       </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,11 @@ export default function Home() {
         <main className="flex-grow container mx-auto gap-10 p-2 flex">
           <div className="w-1/2 flex flex-col items-center">
             <TypewriterEffectDemo className="w-full custom:w-1/2 text-sm custom:text-lg" />
-            <button className="bg-blue-300 hover:bg-blue-500 text-white px-6 py-3 rounded-full font-bold shadow-lg">
-              Voir notre collection
-            </button>
+            <a href="https://www.epagine.fr/listeliv.php?refgtl=home&base=ebook&select_tri_recherche=&codegtl1=1000000&codegtl2=1010000&rayon=Litt%26eacute%3Brature+g%26eacute%3Bn%26eacute%3Brale%7CRomans+%26amp%3B+Nouvelles">
+              <button className="bg-blue-300 hover:bg-blue-500 text-white px-6 py-3 rounded-full font-bold shadow-lg">
+                Voir notre collection
+              </button>
+            </a>
             <div className="mt-4 text-yellow-500">
               ★★★★★ +500 avis positifs
             </div>
